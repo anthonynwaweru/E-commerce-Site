8c84cae947caeda3e844183e0a9f531e83b35f15
anthonypmarcel <anthonypmarcel@gmail.com>
2021-12-17 13:50:28 +0100

    Final commit

--- script.js
+++ script.js
@@ -61,7 +61,9 @@ const showSlides = function (n) {
   for (let i = 0; i < photoThumbs.length; i++) {
     photoThumbs[i].classList.remove('active');
   }
-
+  // document.querySelector('.main-image').src = `images/image-product-${
+  //   slideindex - 1
+  // }.jpg`;
   photoSlides[slideindex - 1].style.display = 'block';
   photoThumbs[slideindex - 1].classList.add('active');
 };
@@ -89,44 +91,85 @@ document.querySelector('.next').addEventListener('click', function () {
   slidePlus(1);
 });
 
+const mobileSlides = document.querySelectorAll('.main-image');
+const mainPhoto = document.getElementById('mainPhoto');
+let currMoSlide = 1;
+
+// Mobile slider
+document
+  .querySelector('.previous-mobile')
+  .addEventListener('click', function () {
+    if (currMoSlide >= 1) currMoSlide--;
+    mainPhoto.src = `images/image-product-${currMoSlide}.jpg`;
+    // mobileSliderComponent();
+    if (currMoSlide < 1) {
+      currMoSlide = 4;
+
+      mainPhoto.src = `images/image-product-${currMoSlide}.jpg`;
+    }
+  });
+
+document.querySelector('.next-mobile').addEventListener('click', function () {
+  if (currMoSlide <= 4 || currMoSlide === 0) {
+    currMoSlide++;
+    console.log(currMoSlide);
+    mainPhoto.src = `images/image-product-${currMoSlide}.jpg`;
+  }
+  if (currMoSlide > 4) {
+    currMoSlide = 1;
+    mainPhoto.src = `images/image-product-${currMoSlide}.jpg`;
+  }
+});
+
 // updating counter
 let count = 0;
+let totalAmount = 0;
 
-const updateCount = function (count) {
-  document.querySelector('.no--pairs').innerHTML = count;
+const updateCount = function (n) {
+  if (n > 1) {
+    document.querySelector('.minus').classList.remove('hidden');
+    document.querySelector('.no--pairs').innerHTML = n;
+  }
+  if (Number(n) === 1) {
+    document.querySelector('.minus').classList.add('hidden');
+    document.querySelector('.no--pairs').innerHTML = n;
+  } else {
+    document.querySelector('.no--pairs').textContent = n;
+  }
 };
 
 //minus
 minus.addEventListener('click', function () {
-  if (count > 0) {
+  if (count > 1) {
     count--;
     updateCount(count);
-  } else if (count === 0) {
-    return;
   }
 });
 //add
 plus.addEventListener('click', function () {
-  count++;
+  if (count > 1) {
+    document.querySelector('.minus').classList.remove('hidden');
+  } else if (count === 1) {
+    document.querySelector('.minus').classList.add('hidden');
+  }
+  count += 1;
+
   updateCount(count);
 });
 
 // adding items to cart and Setting cart state
 let cartState;
+let shoesBought = 0;
 
 document
   .querySelector('.btn--addtocart')
   .addEventListener('click', function () {
-    let totalAmount;
+    // let totalAmount;
     let price = 125.0;
-    if (count === 0) {
-      emptyCartModal.classList.remove('hidden');
-      setTimeout(function () {
-        emptyCartModal.classList.add('hidden');
-      }, 2000);
-    }
-    if (count > 0) {
-      totalAmount = price * count;
+    count = Number(document.querySelector('.no--pairs').textContent);
+    if (count >= 1) {
+      shoesBought += count;
+      totalAmount = totalAmount + price * shoesBought;
       document.querySelector(
         '.filled-cart'
       ).innerHTML = `<h1 class="cartheading">Cart</h1><span class="closeModal">&times;</span>
@@ -141,31 +184,32 @@ document
       </div>
       
         <button class="checkout--btn" type="button">Checkout</button>`;
-      document.querySelector('.itemCartBtn').innerHTML = count;
+      // count = updateCount(count);
+      document.querySelector('.itemCartBtn').innerHTML = shoesBought;
       document.querySelector('.itemCartBtn').classList.remove('hidden');
     }
   });
 
 document.querySelector('.navcart').addEventListener('click', function () {
   console.log(count);
-  if (count === 0) {
-    cartState = false;
-    if (!cartState) {
-      emptyCartModal.classList.remove('hidden');
-      emptyCartModal.addEventListener('click', function (e) {
-        if (e.target.classList.contains('closeModal')) {
-          emptyCartModal.classList.add('hidden');
-        }
-      });
-      // setTimeout(function () {
-      //   emptyCartModal.classList.add('hidden');
-      // }, 3000);
-    }
+  console.log(document.querySelector('.itemCartBtn'));
+  if (Number(document.querySelector('.itemCartBtn').textContent) === 0) {
+    emptyCartModal.classList.remove('hidden');
+    emptyCartModal.addEventListener('click', function (e) {
+      if (e.target.classList.contains('closeModal')) {
+        emptyCartModal.classList.add('hidden');
+      }
+    });
+    setTimeout(function () {
+      emptyCartModal.classList.add('hidden');
+    }, 3000);
   }
-  if (count > 0) {
+  if (Number(document.querySelector('.itemCartBtn').textContent) === 0) {
     cartState = true;
-    if (cartState) filledCart.classList.remove('hidden');
+  } else {
+    cartState = false;
   }
+  if (!cartState) filledCart.classList.remove('hidden');
 });
 
 document.querySelector('.filled-cart').addEventListener('click', function (e) {
@@ -173,25 +217,42 @@ document.querySelector('.filled-cart').addEventListener('click', function (e) {
     filledCart.classList.add('hidden');
   }
   if (e.target.classList.contains('delete')) {
-    count = 0;
+    count = 1;
+    shoesBought = 0;
     updateCount(count);
     document.querySelector('.itemCartBtn').classList.add('hidden');
     filledCart.innerHTML = `<h1 class="cartheading">Cart</h1><span class="closeModal">&times;</span>
     <div class="container cart-divider"></div>
     <p class="empty--cart">Item removed from the cart.</p>`;
     setTimeout(function () {
+      document.querySelector('.itemCartBtn').textContent = '0';
       filledCart.classList.add('hidden');
     }, 3000);
   }
   if (e.target.classList.contains('checkout--btn')) {
-    count = 0;
+    count = 1;
+    shoesBought = 0;
     updateCount(count);
     document.querySelector('.itemCartBtn').classList.add('hidden');
     filledCart.innerHTML = `<h1 class="cartheading" style="color: #ff7e1b;">Order Placed</h1><span class="closeModal">&times;</span>
     <div class="container cart-divider"></div>
     <p class="empty--cart" style="color: #ff7e1b;">Thank you for shopping at Sneakers.</p>`;
     setTimeout(function () {
+      document.querySelector('.itemCartBtn').textContent = '0';
       filledCart.classList.add('hidden');
+      // location.reload();
     }, 3000);
   }
 });
+
+// activating the hamburger menu
+
+document.querySelector('.menu').addEventListener('click', function () {
+  document.querySelector('.hamburger--menu').classList.remove('hidden');
+});
+
+document.querySelector('.menu-star').addEventListener('click', function () {
+  document.querySelector('.hamburger--menu').classList.add('hidden');
+});
+
+// mobile slides
